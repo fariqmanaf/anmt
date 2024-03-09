12c200b1e88690c4d9cee871a72f64e93d8d56ed
fariqmanaf <fariqmanaf24@gmail.com>
2024-03-09 14:11:47 +0700

    k

--- app.js
+++ app.js
@@ -13,10 +13,15 @@ imageLogo.onmouseleave = () => {
 }
 
 
+span.onmouseleave = () => {
+    gsap.to("#rotate", { rotation: 0, x: 0, yPercent: 0, duration: 0.7})
 span.onmouseenter = () => {
     gsap.to("#rotate", { rotation: 360, x: 0, yPercent: 0, duration: 0.7})
-}
+}}
 
-gsap.from("#img-content", {opacity:0,x: -200, duration: 0.6, ease: "power1.inOut"})
+
+gsap.from("#img-content", {opacity:0,x: -300, duration: 0.4, ease: "power1.inOut"})
 gsap.from("#text-container", {opacity:0,x: 200, duration: 1, ease: "power1.inOut"})
 gsap.from(".nav-container", {opacity:0,y: -100, duration: 1, ease: "power1.inOut"})
+
+document.getElementById("img-content").onmouseenter.style.hover.scale = 1.1
